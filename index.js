feed362574cf10fd6ce0f53349a72ca021f59a08
Ryan M <ryan.a.moises@gmail.com>
2024-03-07 17:23:59 -0500

    Generation of SVG is finished; TODO: test suites

--- index.js
+++ index.js
@@ -1,64 +1,122 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const { join } = require('path');
-const shapes = require('./lib/shapes.js')
+const { Triangle, Square, Circle } = require('./lib/shapes.js');
 
 inquirer.registerPrompt('maxlength-input', require('inquirer-maxlength-input-prompt'));
 
-const getHex = async (getText) => {
-    if (getText.color === 'Other') {
+// Gathers user's preferred color hex code or translates a keyword into a hex code
+const getHex = async (e) => {
+    if (e.color === 'Other (Use Hex)') {
         let hex = await inquirer.prompt(
             {
                 type: 'maxlength-input',
-                message: 'Enter hex',
+                message: 'Enter hex-code for preferred color',
                 name: 'color',
                 maxLength: 6
             }) 
-            
             return hex.color
-        } 
+
+        } else if (e.color === 'Red') {
+            return 'FF0000'
+        } else if (e.color === 'Green') {
+            return '008000'
+        } else if (e.color ==='Yellow') {
+            return 'FFFF00'
+        } else if (e.color === 'Blue') {
+            return '0000FF'
+        } else if (e.color === 'Purple') {
+            return 	'800080'
+        } else if (e.color ==='Orange') {
+            return 'FFA500'
+        } else if (e.color ==='White') {
+            return 'FFFFFF'
+        } else if (e.color ==='Black') {
+            return '000000'
+        }
+    }
+
+const colorChoices = ['Other (Use Hex)','Red','Green','Yellow','Blue','Purple','Orange','White','Black'];
+
+function writeToFile(fileName, input) {
+
+    let svg = 
+    `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    
+    `
+    let shapeChoice;
+    if (input.shape === 'Circle') {
+        shapeChoice = new Circle;
+        shapeChoice.setColor(input.shapeColor);
+    } else if (input.shape === 'Square') {
+        shapeChoice = new Square;
+        shapeChoice.setColor(input.shapeColor);
+    } else if (input.shape === 'Triangle') {
+        shapeChoice = new Triangle
+        shapeChoice.setColor(input.shapeColor);
+    }
+
+    svg += shapeChoice.render();
+
+    svg += `
+    
+    <text x="150" y="125" text-anchor="middle" font-size="60" font-weight="bold" font-style="italic" fill="#${input.color}">${input.text}</text>`;
+
+    svg += '</svg>';
+
+    fs.writeFile(
+      fileName,
+      svg,
+      (err) => err ? console.log(err) : console.log('Generated '+ fileName)
+      )
     }
 
 async function main() {
-    let getText = await inquirer.prompt([
+    // Prompts for text and text color
+    let text = await inquirer.prompt([      
         {
             type: 'maxlength-input',
             message: 'Enter text for the logo (Up to 3 characters)',
-            name: 'text',
+            name: 'item',
             maxLength: 3
         },
         {
-            type: 'list',
+            type: 'rawlist',
             message: 'Enter a text color',
             name: 'color',
-            choices: ['Red','Green','Other']
+            choices: colorChoices
         }
     ])
+    // Gathers hex color code from user or translate keyword into hex color code
+    let colorHex = await getHex(text);      
 
-    let colorHex = await getHex(getText);
-    getText.color = colorHex;
-
-    console.log(getText);
-
-    let getShape = await inquirer.prompt([
+    // Prompts for shape and shape color
+    let shape = await inquirer.prompt([     
         {
             type: 'list',
             message: "Select a shape for the logo",
-            name: 'shape',
+            name: 'item',
             choices: ['Circle', 'Square', 'Triangle'],
         },
         {
-            type: 'list',
+            type: 'rawlist',
             message: 'Enter a color for the shape',
             name: 'color',
-            choices: ['Red','Green','Other']
+            choices: colorChoices
         }
     ])
+    // Gathers hex color code from user or translate keyword into hex color code
+    let colorHexShape = await getHex(shape);        
 
-    let colorHexShape = await getHex(getShape);
-    getShape.color = colorHexShape;
+    // Consolidates user inputs and hex codes into one object
+    const input = {
+        text: text.item,
+        color: colorHex,
+        shape: shape.item,
+        shapeColor: colorHexShape
+    };
 
-    console.log(getShape);
+    writeToFile('logo.svg', input);
 }
 
-main()
+main();
