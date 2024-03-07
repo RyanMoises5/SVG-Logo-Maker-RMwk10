ea735e363838ce7b7df9b0377183161cc4967d8d
Ryan M <ryan.a.moises@gmail.com>
2024-03-07 15:26:13 -0500

    Testing async/await functions with inquirer

--- index.js
+++ index.js
@@ -3,36 +3,62 @@ const inquirer = require('inquirer');
 const { join } = require('path');
 const shapes = require('./lib/shapes.js')
 
-const questions = [
-    {
-        type: 'input',
-        message: 'Enter text for the logo (Must not be more than 3 characters)',
-        name: 'text'
-    },
-    {
-        type: 'input',
-        message: 'Enter a text color',
-        name: 'color'
-    },
-    {
-        type: 'list',
-        message: "Select a shape for the logo",
-        name: 'shape',
-        choices: ['Circle', 'Square', 'Triangle'],
-    },
-    {
-        type: 'input',
-        message: 'Enter a color for the shape',
-        name: 'shapecolor'
-    }
-]
+inquirer.registerPrompt('maxlength-input', require('inquirer-maxlength-input-prompt'));
 
-inquirer
-    .prompt(questions)
-    .then((response) => {
-        // logic here
-    })
-    .then((response) => {
-        fs.writeFile('./logo.svg');
-        console.log('Generated logo.svg')
-    });
+const getHex = async (getText) => {
+    if (getText.color === 'Other') {
+        let hex = await inquirer.prompt(
+            {
+                type: 'maxlength-input',
+                message: 'Enter hex',
+                name: 'color',
+                maxLength: 6
+            }) 
+            
+            return hex.color
+        } 
+    }
+
+async function main() {
+    let getText = await inquirer.prompt([
+        {
+            type: 'maxlength-input',
+            message: 'Enter text for the logo (Up to 3 characters)',
+            name: 'text',
+            maxLength: 3
+        },
+        {
+            type: 'list',
+            message: 'Enter a text color',
+            name: 'color',
+            choices: ['Red','Green','Other']
+        }
+    ])
+
+    let colorHex = await getHex(getText);
+    getText.color = colorHex;
+
+    console.log(getText);
+
+    let getShape = await inquirer.prompt([
+        {
+            type: 'list',
+            message: "Select a shape for the logo",
+            name: 'shape',
+            choices: ['Circle', 'Square', 'Triangle'],
+        },
+        {
+            type: 'list',
+            message: 'Enter a color for the shape',
+            name: 'color',
+            choices: ['Red','Green','Other']
+        }
+    ])
+
+    let colorHexShape = await getHex(getShape);
+    getShape.color = colorHexShape;
+
+    console.log(getShape);
+}
+
+main()
